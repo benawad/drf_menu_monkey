fb83f95a8768f9eda24676184ef386129337c289
ben awad <benawad97@gmail.com>
2016-08-25 10:56:37 -0500

    Change recipe-ingredient relationship from foreign key to many to many

--- menu_monkey/recipes/models.py
+++ menu_monkey/recipes/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # class Menu(models.Model):
-    # group_name = models.CharField(max_length=200)
+    # menu_name = models.CharField(max_length=200)
     # public = models.BooleanField(default=False)
     # saved = models.ManyToManyField(User, blank=True, related_name='menu_saved')
     # owner = models.ForeignKey(User)
     
     # def __str__(self):
-        # return self.group_name
+        # return self.menu_name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return str(self.name)
+
 
 class Recipe(models.Model):
     # menus = models.ManyToManyField(Menu, blank=True)
@@ -18,16 +27,11 @@ class Recipe(models.Model):
     public = models.BooleanField(default=False)
     saved = models.ManyToManyField(User, blank=True, related_name='recipe_saved')
     owner = models.ForeignKey(User, related_name='recipes')
+    ingredients = models.ManyToManyField(Ingredient, related_name='recipe_ingredients')
 
     def __str__(self):
         return self.title
 
-class Ingredient(models.Model):
-    recipe = models.ForeignKey(Recipe, related_name='ingredients')
-    name = models.CharField(max_length=200)
-
-    def __unicode__(self):
-        return str(self.name)
 
 # class GroceryList(models.Model):
     # ingredients = models.ManyToManyField(Ingredient)
--- menu_monkey/recipes/serializers.py
+++ menu_monkey/recipes/serializers.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    ingredients = serializers.StringRelatedField(many=True)
     class Meta:
         model = Recipe
 
