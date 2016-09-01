e9efa0a37cd7fe47aecb8ce00d23286213a35c1d
ben awad <benawad97@gmail.com>
2016-09-01 09:29:39 -0500

    Keep debug on

--- menu_monkey/settings.py
+++ menu_monkey/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$53y29_x@wh$-+876i(80bj#abb#=qtzp$j0ya-ls35e3vl@k='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
